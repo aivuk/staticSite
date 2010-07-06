93bbbb1f3eb3a5462aab2a800f584c38d8a10a68
Edgar Z. Alvarenga <edgar@ymonad.com>
2010-07-05 23:39:19 -0300

    Cleaning a little the menu code

--- main.hs
+++ main.hs
@@ -37,11 +37,13 @@ dirsDict directory = do
                 Right pf <- parseCSVFromFile (directory ++ "/" ++ d ++ "/menu")
                 let pd = cleanStrings $ filter (/= [""]) pf
                 sd <- forM s $ \s' -> do
-                    Right sf <- parseCSVFromFile (directory ++ "/" ++ d ++ "/" ++ s' ++ "/menu")
+                    Right sf <- parseCSVFromFile (directory ++ "/" ++ 
+                                                  d ++ "/" ++ s' ++ "/menu")
                     return $ (s', cleanStrings $ filter (/= [""]) sf)
                 return $ ((d, pd):sd) ++ l) [] dirs
     let dict = M.fromListWith M.union $ [ (l, M.fromList [ (d, (menu, dir)) ])
-                                        | (d, pd) <- dictData, [l, dir, menu] <- pd ]
+                                        | (d, pd) <- dictData, 
+                                          [l, dir, menu] <- pd ]
     return dict
 
 -- Return the URL's directory in a lang
@@ -60,26 +62,30 @@ menuInLang dirs dict primDir secDir lang = (primMenu ordDirs, secMenu ordDirs)
     linkName d = case M.lookup d (dict M.! lang) of
                     Nothing -> ("?","?")
                     Just ln -> ln
+
     ordDirs = sortBy (\x y -> compare (fst x) (fst y)) dirs
 
-    selectPrimState p | p == primDir  =  " class=\"selected\"" 
-                      | otherwise     =  ""
+    select d p | p == d    =  " class=\"selected\"" 
+               | otherwise =  ""
 
-    selectSecState p | p == secDir  =  " class=\"selected\"" 
-                     | otherwise     =  ""
+    secMenu = concatMap sm  
 
-    primMenu (d:dirs) = ("<li" ++ selectPrimState (fst d) ++ " id=\"" ++ (fst $ linkName $ fst d) ++ "\"><a href=\"#\" onclick=\"javascript:selectMenu('" ++ (fst $ linkName $ fst d) ++ "')\">" ++ (snd $ linkName $ fst d) ++ "</a></li>\n") ++ (pm dirs)
-
-    secMenu (d:dirs) = ("<ul" ++ selectPrimState (fst d) ++ " id=\"" ++ (fst $ linkName $ fst d) ++ "\">\n" ++ (foldl (\s l -> s ++ "<li" ++ selectSecState l ++ ">" ++ slink (fst d) l ++ "</li>\n") "" $ snd d) ++ "</ul>") ++ concatMap sm dirs
-
-    pm = foldl (\s (d,_) -> s ++ "<li id=\"" ++ (fst $ linkName d) ++ "\"><a href=\"#\" onclick=\"javascript:selectMenu('" ++ (fst $ linkName d) ++ "')\">" ++ (snd $ linkName d) ++ "</a></li>\n") ""
-
-    sm = \(prim, sec) -> "<ul id=\"" ++ (fst $ linkName prim) ++ "\"" ++ (foldl (\s d -> s ++ "<li>" ++ slink prim d ++ "</li>\n") "" sec) ++ "</ul>" 
+    sm = \(prim, sec) -> "<ul" ++ select primDir prim ++ " id=\"" ++
+                         (fst $ linkName prim) ++ "\"" ++
+                         (foldl (\s d -> s ++ "<li" ++ select secDir d ++
+                         ">" ++ slink prim d ++ "</li>\n") "" sec) ++ "</ul>"
 
     slink p s | s == secDir  =  snd $ linkName s
-              | otherwise    =  "<a href=\"/" ++ url p s ++ "\">" ++ (snd $ linkName s) ++ "</a>"
+              | otherwise    =  "<a href=\"/" ++ url p s ++ "\">" ++ 
+                                (snd $ linkName s) ++ "</a>"
 
     url prim sec = urlInLang dict prim sec lang
+
+    primMenu = foldl (\s (d,_) -> s ++ "<li" ++ select primDir d  ++ 
+                                   " id=\"" ++ (fst $ linkName d) ++ 
+                                   "\"><a href=\"javascript:selectMenu('" ++ 
+                                   (fst $ linkName d) ++ "')\">" ++ 
+                                   (snd $ linkName d) ++ "</a></li>\n") ""
 
 languages = ["pt", "en", "de"]
 
@@ -105,9 +111,11 @@ main = hakyll "http://it3s.org" $ do
                         menu = createCustomPage "" [ ("menuPrim", Left $ primMenu)
                                                    , ("menuSec", Left $ secMenu) ] 
 
-                        (primMenu, secMenu) = menuInLang dirs dirsDict primaryDir secondaryDir lang
+                        (primMenu, secMenu) = menuInLang dirs dirsDict 
+                                                         primaryDir secondaryDir lang
 
-                        footer   = createPage $ "conteudo/" ++ lang ++ ".footer.markdown"
+                        footer   = createPage $ "conteudo/" ++ lang ++ 
+                                                ".footer.markdown"
 
                         createLangButtons = do
                             langLinks <- forM languages $ \l -> do
@@ -115,12 +123,16 @@ main = hakyll "http://it3s.org" $ do
                                   then return $ "<b>" ++ lang ++ "</b>"
                                   else do
                                     haveFile <- liftIO $ doesFileExist $ textFile l
-                                    if haveFile 
+                                    if haveFile
                                       then do
-                                        return $ "<a href=\"/" ++ (urlInLang dirsDict primaryDir secondaryDir l) ++
+                                        return $ "<a href=\"/" ++
+                                                (urlInLang dirsDict primaryDir
+                                                           secondaryDir l) ++
                                                 "/\">" ++ l ++ "</a>"
                                       else return ""
-                            return $ createCustomPage "" [ ("changeLanguage", Left $ intercalate " | " $ filter (/= "") langLinks) ]
+                            return $ createCustomPage "" [ ("changeLanguage", Left $ 
+                                                           intercalate " | " $ 
+                                                           filter (/= "") langLinks) ]
                     
                     changeLanguage <- createLangButtons
                     haveFile <- liftIO $ doesFileExist $ textFile lang
